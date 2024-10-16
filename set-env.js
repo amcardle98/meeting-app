035f44b48aefd2a121c3ddb3efccb44fc8b41fc8
Alex <alexmcardle51@gmail.com>
2024-10-16 11:19:23 -0400

    environment variables r dum

---
+++ set-env.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const path = require("path");
+const successColor = "\x1b[32m%s\x1b[0m";
+const checkSign = "\u{2705}";
+
+const envFile = `export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "${process.env.FIREBASE_API_KEY}",
+        authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
+        projectId: "${process.env.FIREBASE_PROJECT_ID}",
+        storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
+        messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+        appId: "${process.env.FIREBASE_APP_ID}",
+        measurementId: "${process.env.FIREBASE_MEASUREMENT_ID}"
+    }
+};
+`;
+const targetPath = path.join(
+  __dirname,
+  "./src/environments/environment.development.ts"
+);
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.development.ts`
+    );
+  }
+});
